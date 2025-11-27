eede0955eac67ad88188c90eb66ae99940c01199
Ewenne <221577967+ew-enne@users.noreply.github.com>
2025-11-27 12:43:51 +0100

    Add require_relative to main.rb

--- main.rb
+++ main.rb
@@ -1,4 +1,15 @@
-
+require_relative 'lib/check' # method
+require_relative 'lib/rules'
+require_relative 'lib/game'
+require_relative 'lib/board'
+require_relative 'lib/peg'
+require_relative 'lib/player'
+require_relative 'lib/player/code_breaker'
+require_relative 'lib/player/code_breaker/ai_code_breaker'
+require_relative 'lib/player/code_breaker/human_code_breaker'
+require_relative 'lib/player/code_maker'
+require_relative 'lib/player/code_maker/ai_code_maker'
+require_relative 'lib/player/code_maker/human_code_maker'
 
 
 
